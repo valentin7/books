98faac9b08f48354fa6dc1e9e7fda064c92000c2
Valentin Perez <valpe70@gmail.com>
2015-09-06 05:34:29 -0400

    proof of concept working

---
+++ sketch.js
@@ -0,0 +1,20 @@
+// function setup() {
+//   // uncomment this line to make the canvas the full size of the window
+//   createCanvas(windowWidth, windowHeight);
+
+// }
+
+function setup() {
+  createCanvas(2000, 2000);
+}
+
+function draw() {
+
+
+  // if (mouseIsPressed) {
+  //   fill(0);
+  // } else {
+  //   fill(255);
+  // }
+  // ellipse(mouseX, mouseY, 80, 80);
+}
